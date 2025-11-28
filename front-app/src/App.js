f7f21a3f63aad230ea2551d4b5b08fd3155d2cee
Pauline <pauline.benghouzi@gmail.com>
2025-11-28 14:02:35 +0100

    [UPDATE] App.js Ajout des logs pour l'api gateway

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Notification from "./Notification";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const internalApi = process.env.REACT_APP_API_URL;
+    const browserApi = "http://localhost:5000";
+
+    console.log("Front → API Gateway (Docker internal):", internalApi);
+    console.log("Front → API Gateway (local browser):", browserApi);
+
+    setMessage(`Connecté à l'API Gateway : ${browserApi}`);
+  }, []);
+
   return (
     <div className="App">
+      <Notification message={message} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
