3841f5ec7ce2213c3cbd0e8236849b9260588fb0
Julien BERNARD <160244920+jbernardPro@users.noreply.github.com>
2025-12-15 11:44:09 +0100

    [UPDATE] Auto create table at db init

--- commands-service/create_commands_table.sql
+++ commands-service/create_commands_table.sql
@@ -3,17 +3,13 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE EXTENSION IF NOT EXISTS "citext";
 
--- Table des commandes
 CREATE TABLE IF NOT EXISTS commands (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
     total_amount NUMERIC(10,2) NOT NULL CHECK (total_amount >= 0),
     status TEXT NOT NULL DEFAULT 'pending', -- pending / paid / shipped / delivered / cancelled
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-
-    CONSTRAINT fk_commands_user
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
@@ -25,7 +21,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS set_timestamp ON commands;
-
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON commands
 FOR EACH ROW
