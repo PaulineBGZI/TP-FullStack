832e01d80073e27ec017c2a1ef9e745259e3df6a
Pauline <pauline.benghouzi@gmail.com>
2025-12-16 09:26:21 +0100

    [UPDATE] App.js & App.css Suppression du code d'origine

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Notification from "./Notification";
+import Home from "./components/Home";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -19,21 +19,7 @@ function App() {
   return (
     <div className="App">
       <Notification message={message} />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
