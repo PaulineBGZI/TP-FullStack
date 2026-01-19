c0898e3cd1c80edf6fe42c74e94bcea721902413
Pauline <pauline.benghouzi@ynov.com>
2026-01-19 12:05:30 +0100

    [Update] Home.js Ajout de la route du bouton panier

--- front-app/src/components/Home/Home.js
+++ front-app/src/components/Home/Home.js
@@ -39,7 +39,7 @@ function Home() {
           <div className="chip chip-2">Vos avis</div>
           <div className="chip chip-3" onClick={() => navigate("/concept")}>Le<br />concept</div>
           <div className="chip chip-1" onClick={() => navigate("/cookies")}>Nos<br />cookies</div>
-          <div className="chip chip-4">Panier</div>
+          <div className="chip chip-4" onClick={() => navigate("/panier")}>Panier</div>
 
         </div>
       </div>
